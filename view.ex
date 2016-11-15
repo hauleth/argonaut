d39e020b04a860911d5e6ec2732f014f0da63707
Łukasz Jan Niemier <lukasz@niemier.pl>
2016-11-15 18:26:16 +0100

    Configure deployment

--- view.ex
+++ view.ex
@@ -13,18 +13,62 @@ defmodule Argonaut.View do
     plural = opts[:plural] || :"#{singular}s"
 
     quote do
-      def render("index.json", %{unquote(plural) => items} = map) do
-        %{
-          data: render_many(items, __MODULE__, "#{unquote(singular)}.json"),
-          meta: map[:meta] || %{},
-        }
+      require Argonaut.View
+
+      @behaviour Argonaut.View
+
+      def render("index.json", %{unquote(plural) => items, conn: conn} = extra) do
+        Argonaut.View.render(conn, items, __MODULE__, extra)
       end
-      def render("show.json", %{unquote(singular) => item} = map) do
-        %{
-          data: render_one(item, __MODULE__, "#{unquote(singular)}.json"),
-          meta: map[:meta] || %{},
-        }
+      def render("show.json", %{unquote(singular) => item, conn: conn} = extra) do
+        Argonaut.View.render(conn, item, __MODULE__, extra)
       end
+
+      def render("item.json", %{item: item} = map) do
+        %{id: item.id,
+          type: "#{unquote(singular)}",
+          attributes: attributes(item)}
+        |> add_relations(item, item.__struct__.__schema__(:associations))
+      end
+
+      defp add_relations(data, model, relationships) do
+        Enum.reduce(relationships, data, fn (field, data) ->
+          add_relation(data, field, Map.fetch!(model, field))
+        end)
+      end
+
+      defp add_relation(data, field, %Ecto.Association.NotLoaded{}), do: data
+      defp add_relation(data, field, model) do
+        case relation(field, model) do
+          :skip -> data
+          rel -> Map.update(data, :relationships, %{field => rel}, &Map.put_new(&1, field, rel))
+        end
+      end
+
+      def relation(_, _), do: :skip
+
+      defoverridable relation: 2
     end
   end
+
+  def render(conn, data, module, extra) when is_list(data) do
+    %{links: apply(module, :links, [conn, data]),
+      data: Phoenix.View.render_many(data, module, "item.json", as: :item)}
+    |> meta(extra[:meta])
+  end
+  def render(conn, data, module, extra) do
+    %{links: apply(module, :links, [conn, data]),
+      data: Phoenix.View.render_one(data, module, "item.json", as: :item)}
+    |> meta(extra[:meta])
+  end
+
+  def meta(data, nil), do: data
+  def meta(data, %{} = extra) do
+    Map.put(data, :meta, extra)
+  end
+
+  @callback attributes(map()) :: map()
+  @callback relation(atom(), map()) :: map() | :skip
+  @callback links(map(), map() | list()) :: map()
+  @optional_callbacks relation: 2
 end

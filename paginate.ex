548ff06864f87d97fe1bbf3345ec2cfc58d6872d
Łukasz Jan Niemier <lukasz@niemier.pl>
2017-03-07 16:30:18 +0100

    Fix pagination

--- paginate.ex
+++ paginate.ex
@@ -1,25 +1,38 @@
 defmodule Argonaut.Paginate do
-  import Ecto.Query, only: [where: 3, limit: 2]
+  require Ecto.Query
 
-  def since(query, params, column \\ :inserted_at, batch \\ 20)
-  def since(query, %{"last" => last}, column, batch) do
+  def next(query, params, order \\ {:desc, :inserted_at}, batch \\ 20)
+  def next(query, %{"last" => last}, order, batch) do
+    ordering = [order]
+
     query
-    |> where([f], field(f, ^column) < ^last)
-    |> limit(^batch)
+    |> Ecto.Query.order_by(^ordering)
+    |> where(order, last)
+    |> Ecto.Query.limit(^batch)
   end
-  def since(query, _, _, batch) do
+  def next(query, _, order, batch) do
+    ordering = [order]
+
     query
-    |> limit(^batch)
+    |> Ecto.Query.order_by(^ordering)
+    |> Ecto.Query.limit(^batch)
   end
 
-  def pagination_header(conn, items, column \\ :inserted_at)
-  def pagination_header(conn, [], _), do: conn
-  def pagination_header(%Plug.Conn{} = conn, items, column) when is_list(items) do
+  def header(conn, items, column \\ :inserted_at)
+  def header(conn, [], _), do: conn
+  def header(%Plug.Conn{} = conn, items, column) when is_list(items) do
     last = items
            |> List.last
            |> Map.get(column)
 
     Plug.Conn.put_resp_header(conn, "x-last", to_string(last))
   end
-  def pagination_header(conn, _, _), do: conn
+  def header(conn, _, _), do: conn
+
+  defp where(query, {:desc, column}, last) do
+    Ecto.Query.where(query, [f], field(f, ^column) < ^last)
+  end
+  defp where(query, {:asc, column}, last) do
+    Ecto.Query.where(query, [f], field(f, ^column) > ^last)
+  end
 end
